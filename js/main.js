147036bd8583bdf883998bdc447aaa219a220ef4
kaltuumo <kaltuumo905@gmail.com>
2023-02-07 10:54:49 -0800

    html & css

---
+++ js/main.js
@@ -0,0 +1,81 @@
+// console.log("Hi sister and How Are You");
+
+//================================================================
+//variables
+// let name = "kaltumo";
+// let age = 2003;
+// const year = 2022; 
+// console.log( year - age );
+// console.log(name, age);
+
+//================================================================
+//arithmetic operator
+// let x = 5;
+// let y = 10;
+// let result =x*y;
+// console.log(result);
+// console.log(x/y);
+
+//================================================================
+//comparison opertor
+// let var1 = 3;
+// let var2 = 4;
+// console.log(4 > var2)
+
+//================================================================
+// logical operators
+// let var1 = true && true;
+// let var2 = false || false;
+// let var3 = !true;
+// console.log(var1);
+// console.log(var2);
+// console.log(var3);
+
+//================================================================
+//Control flow
+// let per = 69;
+// if(per > 90){
+//     console.log("A+");
+// }else if(per > 80){
+//     console.log("B+");
+// }else if(per > 70){
+//     console.log("C+");
+// }else{
+//     console.log("Lose..........");
+// }
+
+//================================================================
+//Switches to
+// let expr = "Mangoesd";
+// switch(expr){
+//     case "Apple":
+//         console.log("Apple is $1");
+//         break;
+//     case "Mangoes":
+//         console.log("Mangoes is $2");
+//         break;
+//     default:
+//         console.log("Sorry not Store For That" +expr); 
+// }
+
+//================================================================
+//For Loop 
+// for(let i = 0; i < 10; i++){
+//     console.log(i);
+// }
+
+//================================================================
+//Do While loop
+// let i = 0;
+// do {
+//     i +=1;
+//     console.log(i);
+// } while(i<5); 
+//================================================================
+//while
+
+let i =0;
+while(i <10){
+    i++;
+    console.log(i);
+}
